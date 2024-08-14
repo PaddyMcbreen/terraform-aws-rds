6cca772b9beb6160b8cb2c2c6e13ea1de707bf97
Paddy McBreen <paddymcbreen@CB-L132.local>
2024-08-14 11:58:50 +0100

    Refactoring providers code block and implmenting varables to use tf acc creds

--- examples/blue-green-deployment/versions.tf
+++ examples/blue-green-deployment/versions.tf
@@ -1,10 +1,15 @@
 terraform {
-  required_version = ">= 1.0"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.59"
+      version = "~> 5.0"
     }
   }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-west-2"
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
